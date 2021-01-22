a45f635e04e9a4d3a96c16041df4a879879f1d3c
Lei <ratrateroo@gmail.com>
2021-01-22 21:59:28 +0800

    fixed db issues

--- client/my-client/src/App.js
+++ client/my-client/src/App.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 const App = () => {
+	const [username, setUsername] = useState();
+	const [message, setMessage] = useState();
+
+	const onUsernameInputHandler = (event) => {
+		const username = event.target.value;
+		setUsername(username);
+	};
+	const onMessageInputHandler = (event) => {
+		const message = event.target.value;
+		setMessage(message);
+	};
 	const submitHandler = (event) => {
 		event.preventDefault();
+		console.log(username, message);
+		axios
+			.post('http://localhost:3002/api/insert', {
+				username: username,
+				message: message,
+			})
+			.then((result) => {
+				console.log(result);
+			});
 		console.log('Cliked Submit');
 	};
 	return (
 		<div>
 			<form onSubmit={submitHandler}>
 				<label htmlFor="name">Name:</label>
-				<input type="text" name="text" />
+				<input type="text" name="text" onChange={onUsernameInputHandler} />
 				<label htmlFor="name">Message:</label>
-				<input type="text" name="message" />
+				<input
+					type="text"
+					name="message"
+					onChange={onMessageInputHandler}
+				/>
 				<button type="submit">Submit</button>
 			</form>
 		</div>
--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const bodyparser = require('body-parser');
 const mysql = require('mysql');
 
 const app = express();
@@ -10,15 +10,37 @@ const db = mysql.createPool({
 	password: '',
 	database: 'cruddb1',
 });
-
-app.get('/', (req, res) => {
-	// const sqlInsert =
-	// 	"INSERT INTO usermessages (username, usermessage) VALUES ('rose', 'let''s play with fire');";
-	// db.query(sqlInsert, (error, result) => {
-	// 	res.send('Accessing Server.');
-	// });
+app.use(express.json());
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
 });
 
+app.use(bodyparser.urlencoded({ extended: true }));
+
+//app.get('/', (req, res) => {
+// const sqlInsert =
+// 	"INSERT INTO usermessages (username, usermessage) VALUES ('rose', 'let''s play with fire');";
+// db.query(sqlInsert, (error, result) => {
+// 	res.send('Accessing Server.');
+// });
+//});
+
+app.post('/api/insert', (req, res) => {
+	console.log(req);
+	const username = req.body.username;
+	const message = req.body.message;
+	const sqlInsert =
+		'INSERT INTO usermessages (username, usermessage) VALUES (?,?);';
+	console.log(username, message);
+	db.query(sqlInsert, [username, message], (error, result) => {
+		res.send({ username: username, message: message });
+	});
+});
 app.listen(3002, () => {
-	console.log('Server is running on port 3001');
+	console.log('Server is running on port 3002');
 });
